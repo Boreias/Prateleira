87a778eb283dfee75639ae6915a4b6d46d427b5f
Boreias <danielroberto95@hotmail.com>
2025-06-01 16:11:48 -0300

    Resetar o backend

--- back-end/target/debug/deps/back_end-e9298732eca0d8bc.d
+++
@@ -1,5 +0,0 @@
-/media/daniel/SSD/Arquivos/Exercícios Programação/GitHub/Prateleira/back-end/target/debug/deps/libback_end-e9298732eca0d8bc.rmeta: src/main.rs
-
-/media/daniel/SSD/Arquivos/Exercícios Programação/GitHub/Prateleira/back-end/target/debug/deps/back_end-e9298732eca0d8bc.d: src/main.rs
-
-src/main.rs:
